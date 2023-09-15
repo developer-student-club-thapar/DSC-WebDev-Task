51ef613a7bba8b39259b752a8f3128c0cf6f81ab
loki <uddeeptaraajkashyap@gmail.com>
2023-09-15 17:07:25 +0530

    minor revisions

--- Recruitment-Site-Backend/middlewares/auth.js
+++
@@ -1,7 +0,0 @@
-const jwt = require('jsonwebtoken')
-
-const auth = async (req, res, next) => {
-
-}
-
-module.exports = auth
--- Recruitment-Site-Backend/routes/adminRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { regAdmin, loginAdmin } = require('../controllers/adminController');
-
-router.post('/register', regAdmin);
-router.post('/login', loginAdmin);
-
-module.exports = router;
