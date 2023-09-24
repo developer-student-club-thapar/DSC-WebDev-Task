4bb6bca4e26c3329cc5290b3ffe64849e820cb87
kofta999 <mostafaxxx555@gmail.com>
2023-09-24 18:55:21 +0300

    fixed a bug in dbConfig.js

--- Backend-Task/config/dbConfig.js
+++ Backend-Task/config/dbConfig.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-main().catch(err => console.log(err));
-
 const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/recruitment-site';
+
+main().catch(err => console.log(err));
 
 async function main() {
     await mongoose.connect(uri)
