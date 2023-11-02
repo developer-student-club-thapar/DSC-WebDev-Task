fc8d7e5e91a93ee3593114bf89301f4f99e5284c
Sxhail <147580291+Sxhail@users.noreply.github.com>
2023-11-02 10:10:02 +0530

    Added cookie parser
    - Added the `cookie-parser` middleware to handle cookies in the application.
    - Configured the middleware to parse cookies and make them accessible to routes.

--- Backend-Task/index.js
+++ Backend-Task/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser'); // Import the cookie-parser middleware
 const dbConfig = require('./config/dbConfig');
 
 const userRoute = require('./routes/userRoute');
@@ -14,6 +15,7 @@ dbConfig();
 // Middleware
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser()); // Add cookie-parser as middleware
 
 // Routes
 app.use('/api/user', userRoute);
@@ -21,6 +23,6 @@ app.use('/api/admin', adminRoute);
 
 // Listen to port
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, ()=> {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 })
